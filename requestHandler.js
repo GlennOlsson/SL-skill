e7055f853f3af9221c6bf5da338b81807112aa3f
Glenn Olsson <glennholsson@gmail.com>
2020-10-25 15:56:55 +0100

    Update error response

--- requestHandler.js
+++ requestHandler.js
@@ -4,24 +4,33 @@ function handleIntent(json) {}
 
 function handleBadRequest(reasonTitle, reasonDesc) {
 	const errorBody = {
-		version: "1.0",
 		response: {
 			outputSpeech: {
 				type: "SSML",
-				text: "<speak><amazon:emotion name='disappointed' intensity='high'> There was an error with the request. See the app for more information</amazon:emotion></speak>",
+				ssml:
+					"<speak><amazon:emotion name='disappointed' intensity='high'> There was an error with the request. See the app for more information</amazon:emotion></speak>",
 			},
-			// card: {
-			// 	type: "Simple",
-			// 	title: reasonTitle,
-			// 	content: reasonDesc,
-			// },
+			card: {
+				type: "Simple",
+				title: reasonTitle,
+				content: reasonDesc,
+			},
+			reprompt: {
+				outputSpeech: {
+					type: "SSML",
+					ssml: "<speak>Go ahead and say hello to me!</speak>",
+				},
+			},
+			shouldEndSession: false,
 		},
+		version: "1.0",
+		sessionAttributes: {},
 	};
 	return errorBody;
 }
 
 module.exports = {
-	handleLaunch, 
-	handleIntent, 
-	handleBadRequest
-}
+	handleLaunch,
+	handleIntent,
+	handleBadRequest,
+};
